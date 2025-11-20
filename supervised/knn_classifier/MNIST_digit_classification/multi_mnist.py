52ec3a4d26f77c2e587c43513061fa9028402e46
HarshKorde5 <harshkorde05@gmail.com>
2025-11-20 15:36:58 +0530

    add : multiprocessing for k_values

---
+++ supervised/knn_classifier/MNIST_digit_classification/multi_mnist.py
@@ -0,0 +1,61 @@
+import time
+import multiprocessing as mp
+from tensorflow.keras.datasets import mnist
+from sklearn.neighbors import KNeighborsClassifier
+
+X_TRAIN = None
+Y_TRAIN = None
+X_TEST = None
+Y_TEST = None
+"""
+globals are most important incase of multiprocessing
+multiple process duplicate our dataset n times
+this creates n copies of same dataset increasing the memory usage unecessarily
+declaring global will initialize the memory only once and use it in every process
+"""
+def init_globals(x_train, y_train, x_test, y_test):
+    global X_TRAIN, Y_TRAIN, X_TEST, Y_TEST
+    X_TRAIN = x_train
+    Y_TRAIN = y_train
+    X_TEST = x_test
+    Y_TEST = y_test
+    
+
+
+def evaluate_k(k):
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_TRAIN, Y_TRAIN)
+    accuracy = knn.score(X_TEST, Y_TEST)*100 
+    return k, accuracy
+    
+    
+def main():
+    global X_TRAIN, Y_TRAIN, X_TEST, Y_TEST
+
+    (X_TRAIN, Y_TRAIN), (X_TEST, Y_TEST) = mnist.load_data()
+
+    X_TRAIN = X_TRAIN.reshape(len(X_TRAIN), -1) / 255.0
+    X_TEST = X_TEST.reshape(len(X_TEST), -1) / 255.0
+        
+    # print("Number of cores : ",mp.cpu_count())    #8
+    
+    k_values = [3, 5, 7, 9, 11]
+    
+    start_time = time.perf_counter()
+    
+    with mp.Pool(processes=mp.cpu_count(),
+                 initializer=init_globals,
+                 initargs=(X_TRAIN, Y_TRAIN, X_TEST, Y_TEST)
+                )as pool:
+        results = pool.map(evaluate_k, k_values)
+        
+    end_time = time.perf_counter()
+        
+    for k,acc in results:
+        print(f"Accuracy with n_neighbors = {k} is : {acc}")
+    
+    # execution time lowers to around 70 seconds than previous 160seconds in mnist_04.py
+    print(f"Execution time : {end_time - start_time}")
+    
+if __name__ == "__main__":
+    main()
